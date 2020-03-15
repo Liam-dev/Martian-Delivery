7759a8fbb7a5f3cdcc250628efb0b0f26083c0cf
Liam-dev <61555544+Liam-dev@users.noreply.github.com>
2020-03-15 18:09:57 +0000

    Update oop.py

--- research examples/oop.py
+++ research examples/oop.py
@@ -1,12 +1,17 @@
+#Animal class
 class Animal:
 
+    #constructor
     def __init__(self, name : str, species : str, age : int):
+        #attributes
         self.name : str = name
         self.species : str = species
         self.age : int = age
     
+    #method
     def hello(self):
         return ("My name is {0} and I am a {1} ".format(self.name, self.species))
 
+#instantiation
 bob : Animal = Animal("Bob", "Hippo", 10)
 print(bob.hello())
