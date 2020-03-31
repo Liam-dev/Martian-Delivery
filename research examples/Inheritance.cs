e4c749a2d6a21db9cd25f99159615af440fbbce0
Liam-dev <liam2159@gmail.com>
2020-03-31 14:01:56 +0100

    Done

--- research examples/Inheritance.cs
+++
@@ -1,5 +0,0 @@
-namespace Inheritance
-{
-    class Vehicle{}
-
-}
---
+++ research examples/Inheritance/Examples/Inheritance/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Plane boeing = new Plane("Boeing", "737");
+            boeing.Move();
+            boeing.Fly();
+        }
+    }
+
+}
