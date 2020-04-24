d383db1a446c159fef702f48a53bb697560c1efe
Liam-dev <liam2159@gmail.com>
2020-04-24 14:24:05 +0100

    Escape key setup

---
+++ Mars Mission/scenes/Main.cs
@@ -0,0 +1,28 @@
+using Godot;
+using System;
+
+public class Main : Spatial
+{
+    // Declare member variables here. Examples:
+    // private int a = 2;
+    // private string b = "text";
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        
+    }
+
+//  // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(float delta)
+    {
+        if (Input.IsActionJustPressed("ui_accept"))
+        {
+            GD.Print("Enter key pressed");
+        }
+        else if (Input.IsActionJustPressed("ui_cancel"))
+        {
+            GetTree().Quit();
+        }
+    }
+}
